9afd1641bde54610ebef33e0c2d3826eceecae81
Avi Peer <avipeeree@gmail.com>
2026-03-23 15:50:16 +0200

    writer.sh created

---
+++ finder-app/writer.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+#writefile=/home/vboxuser/yes/wr_file.txt
+#writestr="yes"
+writefile=$1
+writestr=$2
+#check if 2 arguments were passed
+if ! [ $# -eq 2 ]
+then
+    #if an arguemnt is not passed it's an empry str
+    #-z checks if the string len is 0(empty str)
+    if [ -z "$writefile" ] 
+    then
+    echo "arg1 writefile is missing!"
+    fi
+    if [ -z "$writestr" ]
+    then
+    echo "arg2 writestr is missing!"
+    fi
+    exit 1
+fi
+#prints the string, uses the file location as output
+echo "$writestr" > $writefile
+if [ ! -e $writefile ]
+then
+    echo "File was not created."
+    exit 1
+fi
